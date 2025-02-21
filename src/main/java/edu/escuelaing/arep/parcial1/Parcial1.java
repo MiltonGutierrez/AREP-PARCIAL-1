3bde2cb98da2d39da16e959341edab9d5584b02c
unknown <milton.gutierrez-l@labinfo.is.escuelaing.edu.co>
2025-02-21 10:27:23 -0500

    primer

---
+++ src/main/java/edu/escuelaing/arep/parcial1/Parcial1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.escuelaing.arep.parcial1;
+
+/**
+ *
+ * @author milton.gutierrez-l
+ */
+public class Parcial1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
