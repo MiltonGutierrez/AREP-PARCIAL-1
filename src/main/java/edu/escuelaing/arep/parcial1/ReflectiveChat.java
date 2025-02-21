1fab74efba35505f4a00d9326d47ca493031e28a
MiltonGutierrez <gutierrezmilton31@gmail.com>
2025-02-21 11:23:41 -0500

    class

--- src/main/java/edu/escuelaing/arep/parcial1/ReflectiveChat.java
+++ src/main/java/edu/escuelaing/arep/parcial1/ReflectiveChat.java
@@ -3,9 +3,12 @@ package edu.escuelaing.arep.parcial1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
+import java.util.Arrays;
 
 public class ReflectiveChat {
 
@@ -48,6 +51,51 @@ public class ReflectiveChat {
     private static void handleChatRequest(PrintWriter out, URI resource)throws Exception{
         String fullFunction = resource.getQuery().split("=")[1];
         System.out.println("FULL FUNCTION " + fullFunction);
+        int index = fullFunction.indexOf("(");
+        String function = fullFunction.substring(0, index).toLowerCase();
+        String[] values = fullFunction.substring( index + 1, fullFunction.length() -1).split(",");
+
+        System.out.println("FUNCTION " + function + " parameters " + Arrays.toString(values));
+        String result = "";
+        if(function.equals("class")){
+            result = classFunction(values);
+        }
+        else if(function.equals("invoke")){
+            result = invoke(values);
+        }
+        else if(function.equals("unaryinvoke")){
+            result = unaryInvoke(values);
+        }
+        else if(function.equals("binaryinvoke")){
+            result = binaryInvoke(values);
+        }
+        
+        
+    }
+
+    public static String classFunction(String[] parameters) throws Exception{
+        String className = parameters[0];
+        Class<?> c = Class.forName(className);
+        StringBuilder response = new StringBuilder();
+        Field[] fields = c.getDeclaredFields();
+        Method[] methods = c.getDeclaredMethods();
+        response.append("{\"fields\":"+ "\""+Arrays.toString(fields) + "\"" + ",");
+        response.append("{\"methods\""+"\"" +Arrays.toString(methods) + "\"" +"}");
+        System.out.println(response.toString());
+
+        return response.toString();
+    }
+
+    public static String invoke(String[] parameters){
+        return "";
+    }
+
+    public static String unaryInvoke(String[] parameters){
+        return "";
+    }
+
+    public static String binaryInvoke(String[] parameters){
+        return "";
     }
 
 }
